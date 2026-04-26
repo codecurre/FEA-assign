225d64f4beb0f8e817d8b42f7582bf3fd2de8c3c
Omar Mohamed Sayed Abdelgelel <2200198@eng.asu.edu.eg>
2026-04-26 14:27:15 +0300

    Update assignment.m

--- assignment.m
+++ assignment.m
@@ -2,121 +2,45 @@
 %%MDP411s | intro to finite elements
 %%assignment submission
 
-clc; clear all; close all;
+clc; clear all; close all; %restarting everything
 
 %%Mesh Generation:
 %%---------------------------------------------------------------
 
-coord = csvread('nodes.csv'); %nodes
-elem = csvread('connectivity.csv'); %elements
-nnod = length(coord);
-nel = length(elem);
-ndof = 3*nnod;
+coord = csvread('nodes.csv'); %node matrix which has the coordinates of the node in space with each row index representing one node
+elem = csvread('connectivity.csv'); %elements --which is a matrix that has 2 columns, each row has the two nodes that make up the element
+nnod = length(coord); %this is the number of nodes that we have
+nel = length(elem); %this is the number of elements I had
+ndof = 3*nnod; %the number of degress of freedom in my system
 
 %%parameter definition
 %%---------------------------------------------------------------
 
-FCR_pos = 200; %N
-FDR = 5000; %N
-E = 210e9; %Pa
-R = 20e-3; %m
-A = pi * (R)^2; %m^2
-dX = coord(elem(:,2),1) - coord(elem(:,1),1); %m
-dY = coord(elem(:,2),2) - coord(elem(:,1),2); %m
-dZ = coord(elem(:,2),3) - coord(elem(:,1),3); %m
-L = sqrt(dX.^2 + dY.^2 + dZ.^2); %m
-LMN = [dX./L , dY./L , dZ./L]; %matrix that has l,m,n for each element in the truss
+FCR_pos = 200; % Newton
+FDR = 5000; % Newton
+E = 210e9; % Pascal - assumed to be steel
+R = 2e-2; %meter
+A = pi * (R)^2; %meter^2
+%%below is the
+dX = coord(elem(:,2),1) - coord(elem(:,1),1); %this is the difference between the x of the second node and the x of the first, matrix (nel,1)
+dY = coord(elem(:,2),2) - coord(elem(:,1),2); %this is the difference between the y of the second node and the y of the first, matrix (nel,1)
+dZ = coord(elem(:,2),3) - coord(elem(:,1),3); %this is the difference between the z of the second node and the z of the first, matrix (nel,1)
+L = sqrt(dX.^2 + dY.^2 + dZ.^2); %this is the length of each element put into an (nel,1) matrix
+LMN = [dX./L , dY./L , dZ./L]; %matrix that has l,m,n for each element in the truss ,corresponding to each element, matrix(nel,3)
 
 %%Boundary Conditions:
 %%------------------------------------------------------------------
 
 %from CSV file it is clear that the first 4 nodes are grounded
-fixdof = [1,2,3, 4,5,6, 7,8,9, 10,11,12];
-freedof = setdiff(1:ndof,fixdof);
-Kg = zeros(ndof,ndof); Fg = zeros(ndof,1); Ug = zeros(ndof,1); sigma_g = zeros(nel,1);
-
-%%------------------------------------------------------------------
-%%FORCE LOCATION
-
-
-while true
-  choice = input("enter the case number from 1 to 4(different cases of centrifugal force direction): ");
-  switch choice
-     case 1
-       for i = 5 : nnod
-         X = coord(i,1);
-         Y = coord(i,2);
-         if X == 0 | Y == 0
-           continue
-         else
-           Fg(3*i-1,1) = -FDR;
-           if X > 0
-           Fg(3*i,1) = FCR_pos;
-           else
-           Fg(3*i,1) = -FCR_pos;
-           endif
-         endif
-       end
-       break
-     case 2
-       for i = 5 : nnod
-         X = coord(i,1);
-         Y = coord(i,2);
-         if X == 0 | Y == 0
-           continue
-         else
-           Fg(3*i-1,1) = -FDR;
-           if X > 0
-           Fg(3*i,1) = -FCR_pos;
-           else
-           Fg(3*i,1) = FCR_pos;
-           endif
-         endif
-       end
-       break
-     case 3
-       for i = 5 : nnod
-         X = coord(i,1);
-         Y = coord(i,2);
-         if X == 0 | Y == 0
-           continue
-         else
-           Fg(3*i-1,1) = -FDR;
-           if X > 0
-           Fg(3*i-2,1) = FCR_pos;
-           else
-           Fg(3*i-2,1) = -FCR_pos;
-           endif
-         endif
-       end
-       break
-     case 4
-       for i = 5 : nnod
-         X = coord(i,1);
-         Y = coord(i,2);
-         if X == 0 | Y == 0
-           continue
-         else
-           Fg(3*i-1,1) = -FDR;
-           if X > 0
-           Fg(3*i-2,1) = -FCR_pos;
-           else
-           Fg(3*i-2,1) = FCR_pos;
-           endif
-         endif
-       end
-       break
-     otherwise
-      printf("invalid entry please try again");
-      continue;
-  end
-end
-%%assembly
-
-%TYPE HERE
+fixdof = [1,2,3, 4,5,6, 7,8,9, 10,11,12]; %as the first 12 degrees of freedom(4 nodes) are suppports
+freedof = setdiff(1:ndof,fixdof); %this function creates a matrix which is (1,ndof-fixdof), where the elements in the row are numbers starting from 1 to ndof ,excluding the elements fixdof
+Kg = zeros(ndof,ndof); %intialization of global stiffness matrix
+Fg = zeros(ndof,1); %initialization of global force matrix
+Ug = zeros(ndof,1); %initialization of global displacement matrix
+sigma_g = zeros(nel,1); %initialization of global stress matrix (which is sized (nel,1))
 
 %%-------------------------------------------------------------------
-%%stiffness matrix
+%%stiffness matrix function
 function k = truss_stiff(E,A,L,l,m,n)
 
  k = ((E*A)/L)* [ l^2,    l*m,    l*n,   -l^2,   -l*m,   -l*n;
@@ -126,7 +50,104 @@ function k = truss_stiff(E,A,L,l,m,n)
                 -l*m,   -m^2,   -m*n,    l*m,    m^2,    m*n;
                 -l*n,   -m*n,   -n^2,    l*n,    m*n,    n^2 ];
 endfunction
-%calculating stress for each section
+
+
+
+%%FORCE LOCATION
+%%------------------------------------------------------------------
+
+z_max = max(coord(:,3));
+top_nodes = find(abs(coord(:,3) - z_max) < 1e-6);
+
+while true
+  choice = input("case number: ");
+  switch choice
+     case 1
+       for i = top_nodes'
+         X = coord(i,1);
+         Y = coord(i,2);
+         if X == 0 | Y == 0
+           continue
+         else
+           Fg(3*i-1,1) = -FDR;
+           if X > 0
+             Fg(3*i,1) = FCR_pos;
+           else
+             Fg(3*i,1) = -FCR_pos;
+           endif
+         endif
+       end
+       break
+
+     case 2
+       for i = top_nodes'
+         X = coord(i,1);
+         Y = coord(i,2);
+         if X == 0 | Y == 0
+           continue
+         else
+           Fg(3*i-1,1) = -FDR;
+           if X > 0
+           Fg(3*i,1) = -FCR_pos;
+           else
+           Fg(3*i,1) = FCR_pos;
+           endif
+         endif
+       end
+       break
+
+     case 3
+       for i = top_nodes'
+         X = coord(i,1);
+         Y = coord(i,2);
+         if X == 0 | Y == 0
+           continue
+         else
+           Fg(3*i-1,1) = -FDR;
+           if X > 0
+           Fg(3*i-2,1) = FCR_pos;
+           else
+           Fg(3*i-2,1) = -FCR_pos;
+           endif
+         endif
+       end
+       break
+
+     case 4
+       for i = top_nodes'
+         X = coord(i,1);
+         Y = coord(i,2);
+         if X == 0 | Y == 0
+           continue
+         else
+           Fg(3*i-1,1) = -FDR;
+           if X > 0
+           Fg(3*i-2,1) = -FCR_pos;
+           else
+           Fg(3*i-2,1) = FCR_pos;
+           endif
+         endif
+       end
+       break
+
+     otherwise
+      printf("invalid entry please try again");
+      continue;
+
+  end
+end
+
+
+%%------------------------------------------------------------------------------------
+%%assembly and calculations
+%%------------------------------------------------------------------------------------
+
+
+
+
+%%------------------------------------------------------------------------------------
+%%filling global stiffness matrix
+
 for i = 1 : nel
 
   dofe = [3*elem(i,1)-2 3*elem(i,1)-1 3*elem(i,1) 3*elem(i,2)-2 3*elem(i,2)-1 3*elem(i,2)];
@@ -143,11 +164,21 @@ for i = 1 : nel
 
 end
 
+%%-------------------------------------------------------------------------------------
 %%calculating displacements
 
 Ug(freedof,1) = Kg(freedof,freedof)\Fg(freedof,1);
 
-%stress calculation
+%%-------------------------------------------------------------------------------------
+%%Calculating reaction forces at the supports
+
+Rg = Kg * Ug - Fg;
+
+
+F_reaction = Rg(1:length(fixdof),1);
+
+%%-------------------------------------------------------------------------------------
+%%stress calculation
 for i = 1 : nel
 
   l = LMN(i,1);
@@ -166,12 +197,14 @@ end
 [max_tensile, elem_tensile] = max(sigma_g);
 [max_compressive, elem_compressive] = min(sigma_g);
 
-fprintf('\n--- Stress Results ---\n');
-fprintf('Max Tensile Stress:     %.4f Pa  -> Element %d\n', max_tensile, elem_tensile);
-fprintf('Max Compressive Stress: %.4f Pa  -> Element %d\n', max_compressive, elem_compressive);
+fprintf('------------------------------------------------------\n')
+fprintf('stress results\n');
+fprintf('max tensile stress:     %.4f Pa   element %d\n', max_tensile, elem_tensile);
+fprintf('max compressive stress: %.4f Pa   element %d\n', max_compressive, elem_compressive);
 
-%visualization
-scale = 100;
+%%-----------------------------------------------------------------------------------
+%%visualization
+scale = 10;
 
 def_coord = zeros(size(coord));
 def_coord(:,1) = coord(:,1) + scale * Ug(1:3:end);
@@ -188,10 +221,6 @@ for i = 1:nel
           [coord(n1,2), coord(n2,2)], ...
           [coord(n1,3), coord(n2,3)], 'b');
 end
-
-
-
-
 
 grid on; axis equal;
 xlabel('X (m)'); ylabel('Y (m)'); zlabel('Z (m)');
@@ -234,11 +263,3 @@ title(['Original (blue) vs Deformed (red) — x' num2str(scale) ' scale']);
 legend('Original', 'Deformed');
 view(3);
 hold off;
-
-%Calculating reaction forces at the supports
-
-Rg = Kg * Ug - Fg;
-
-
-F_reaction = Rg(1:12,1);
-
